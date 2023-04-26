5c01498dc221d0519d22b01d3472017f2e27d9cb
jiyuujin <jiyuujinunite@gmail.com>
2023-04-27 06:42:13 +0900

    nuxt v3 stable

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,11 @@
+<script setup lang="ts">
+import NuxtLogo from '~/assets/nuxt_logo.svg'
+</script>
+
 <template>
   <div>
     <div class="title">
-      <img src="~/assets/nuxt_logo.svg" alt="Nuxt logo" class="logo" />
+      <NuxtLogo />
       <p class="title_text">{{ `Nuxt 3` }}</p>
     </div>
   </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,17 @@
-import { defineNuxtConfig } from 'nuxt'
+import svgLoader from 'vite-svg-loader'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   srcDir: 'app/',
-  target: 'static',
   css: ['~/assets/main.css'],
   serverMiddleware: [
     { path: '/api/hello', handler: '~/server/api/hello.ts'},
   ],
-  buildModules: ['@nuxtjs/svg']
+  vite: {
+    plugins: [
+      svgLoader({
+        svgo: false,
+      }),
+    ],
+  },
 })
