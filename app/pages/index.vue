6fa30573f3558f4d5606f81a675858a61b92cc03
YUMA Kitamura <jiyuujinunite@gmail.com>
2023-04-26 18:33:48 +0900

    Initial commit

---
+++ app/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <div class="title">
+      <img src="~/assets/nuxt_logo.svg" alt="Nuxt logo" class="logo" />
+      <p class="title_text">{{ `Nuxt 3` }}</p>
+    </div>
+  </div>
+</template>
